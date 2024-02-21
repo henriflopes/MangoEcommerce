8b1ada023c7f9d1fa9b9273272fc7a421cf12808
Henry Lopes <hlopes-az204@outlook.com>
2024-02-21 17:48:12 -0300

    Add Consume Service Bus Email Queue and ApplicationBuilderExtensions

---
+++ Mango.Services.EmailAPI/Extension/ApplicationBuilderExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Mango.Services.EmailAPI.Messaging;
+
+namespace Mango.Services.EmailAPI.Extension
+{
+	public static class ApplicationBuilderExtensions
+	{
+        private static IAzureServiceBusConsumer _serviceBusConsumer { get; set; }
+
+
+        public static IApplicationBuilder UseAzureServiceBusConsumer(this IApplicationBuilder app)
+        {
+			_serviceBusConsumer = app.ApplicationServices.GetService<IAzureServiceBusConsumer>();
+			var hostApplicationLifer = app.ApplicationServices.GetService<IHostApplicationLifetime>();
+
+			hostApplicationLifer.ApplicationStarted.Register(OnStart);
+			hostApplicationLifer.ApplicationStopping.Register(OnStop);
+
+			return app;
+		}
+
+		private static void OnStop()
+		{
+			_serviceBusConsumer.Stop();
+		}
+
+		private static void OnStart()
+		{
+			_serviceBusConsumer.Start();
+		}
+	}
+}
