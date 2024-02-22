b4182d0b967d77a18e7bceeef565dba9032edf53
Henry Lopes <hlopes-az204@outlook.com>
2024-02-22 17:57:09 -0300

    Added Order functionality

--- Mango.Web/Controllers/CartController.cs
+++ Mango.Web/Controllers/CartController.cs
@@ -1,5 +1,4 @@
-﻿using Mango.Web.Models;
-using Mango.Web.Service.IService;
+﻿using Mango.Web.Models;using Mango.Web.Service.IService;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -10,10 +9,12 @@ namespace Mango.Web.Controllers
 	public class CartController : Controller
 	{
 		private readonly ICartService _cartService;
+		private readonly IOrderService _orderService;
 
-		public CartController(ICartService cartService)
+		public CartController(ICartService cartService, IOrderService orderService)
 		{
 			_cartService = cartService;
+			_orderService = orderService;
 		}
 
 		[Authorize]
@@ -26,6 +27,29 @@ namespace Mango.Web.Controllers
 		public async Task<IActionResult> Checkout()
 		{
 			return View(await LoadCartDtoBasedOnLoggedInUser());
+		}
+
+		[HttpPost]
+		[ActionName("Checkout")]
+		public async Task<IActionResult> Checkout(CartDto cartDto)
+		{
+			CartDto cart = await LoadCartDtoBasedOnLoggedInUser();
+
+			cart.CartHeader.Phone = cartDto.CartHeader.Phone;
+			cart.CartHeader.Email = cartDto.CartHeader.Email;
+			cart.CartHeader.Name = cartDto.CartHeader.Name;
+
+			var response = await _orderService.CreateOrder(cart);
+
+			OrderHeaderDto orderHeaderDto = JsonConvert.DeserializeObject<OrderHeaderDto>(response.Result.ToString());
+
+			if (response != null && response.IsSuccess) 
+			{ 
+				//get stripe session and redirect to stripe to place order
+
+			}
+
+			return View();
 		}
 
 		public async Task<IActionResult> Remove(int cartDetailsId)
